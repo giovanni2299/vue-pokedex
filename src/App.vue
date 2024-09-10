19a1a8a3945c8eae969f3e50176e21e0a653d5fd
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-09-10 15:02:51 +0200

    ADD: all the document and the header

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script >
+import HeaderPage from './components/HeaderPage.vue';
+import MainPage from './components/MainPage.vue';
+ export default{
+  components:{
+    HeaderPage,
+    MainPage
+  },
+  data(){
+    return{
+
+    }
+  }
+ }
+</script>
+
+<template>
+  <HeaderPage/>
+  <MainPage/>
+
+</template>
+
+<style >
+
+</style>
---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,42 @@
+<template>
+    <header>
+        <div class="flex">
+            <img class="logo-ball" src="../../public/Logo-svg.png" alt="">
+
+        </div>
+
+    </header>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+.flex{
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+
+.logo-ball{
+    width: 200px;
+    // height: 100px;
+}
+.logo-text{
+    width: 100%;
+    height: 100%;
+    
+}
+
+header{
+    min-height: 90px;
+    background-color: black;
+}
+
+</style>
